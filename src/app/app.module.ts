17c0d0af3435fb0976cd1cf4c710c67174a3fab9
anisberto <anisbertoos@gmail.com>
2022-02-14 22:16:25 -0300

    criação das paginas principais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './views/pages/footer/footer.component';
+import { TopbarComponent } from './views/pages/topbar/topbar.component';
+import { MainComponent } from './views/pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TopbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
